98d36098dcc74f6e5d9612841c13951d5504e6a2
Oluwagbamila Boluwatife <boluwatt@gmail.com>
2021-08-20 16:06:20 +0100

    completed biodata task

---
+++ Biodata/biodata.js
@@ -0,0 +1,17 @@
+// Storing my Biodata
+let name = "Boluwatife Oluwagbamila";
+let age = "22years";
+let dateOfBirth = "31-07-1999";
+let gender = "Female";
+let employmentStatus = "Unemployed";
+let stateOfOrigin = "Osun State";
+let nationality = "Nigerian";
+
+// Printing my Biodata
+console.log(name);
+console.log(age);
+console.log(dateOfBirth);
+console.log(gender);
+console.log(employmentStatus);
+console.log(stateOfOrigin);
+console.log(nationality);
